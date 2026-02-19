55b37417d2209121e2416a347cae8879b373f477
mohamed aziz  boulifa <boulifa.aziz@esprit.tn>
2026-02-19 02:36:16 +0100

    feat(surgical-dialysis-service): add microservice + eureka registration

---
+++ BackEnd/microservices/surgical-dialysis-service/src/main/java/com/hospital/surgicaldialysisservice/SurgicalDialysisServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.surgicaldialysisservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurgicalDialysisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurgicalDialysisServiceApplication.class, args);
+	}
+
+}
