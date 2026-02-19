1c3a92e9289cbabb198b9ce3550a3b11314c2e74
hamzabelkhouja <belkhouja.hamza@esprit.tn>
2026-02-19 05:43:38 +0100

    Created ehr-staff-management-service

---
+++ BackEnd/microservices/ehr-staff-management-service/src/main/java/com/microservice/ehrstaff/EhrStaffManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.ehrstaff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhrStaffManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhrStaffManagementServiceApplication.class, args);
+	}
+
+}
